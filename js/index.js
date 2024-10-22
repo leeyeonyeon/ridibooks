7bf8d7c987bd0ae152dae415a2bc17d68e2ca990
leeyeonyeon <lys20dus@gmail.com>
2024-10-22 15:55:44 +0900

    수정

--- js/index.js
+++ js/index.js
@@ -7,12 +7,9 @@ $(document).ready(function () {
 
   let bannerWidth = $(".ban ul li").outerWidth(true);
 
-  //목록 마지막 이미지를 목록 안의 가장 앞으로 먼저 배치
   $(".ban ul li:last").prependTo(".ban ul");
-  //첫번째 이미지가 보여야 하므로 앞으로 온 맨뒤 이미지를 왼쪽으로 한칸 밀어두기
   $(".ban ul").css({"left":-bannerWidth+"px"});
 
-  //자동으로 슬라이드 함수생성
   function bannerAuto(){
     $(".ban ul").stop().animate({left:"-="+bannerWidth+"px"},500,function(){
       $(".ban ul li:first-child").appendTo(".ban ul"); //첫번째 이미지가 맨뒤로 이동
@@ -20,8 +17,6 @@ $(document).ready(function () {
     });
   };
 
-  //setInterval(함수명,재생시간); ==> 재생시간 간격으로 함수 자동 실행
-  //clearInterval(변수명); ==> 자동함수를 멈추게 함
   bannerTimer = setInterval(bannerAuto,4000);
 
 
@@ -44,7 +39,7 @@ $(document).ready(function () {
   });
 
 
-  //마우스오버 시 멈춤(좌우버튼을 누를때는 자동함수가 실행이 안 되게 함)
+  //마우스오버 시 멈춤
   $(".ban, .ban_btn .ban_left, .ban_btn .ban_right").hover(function(){
     clearInterval(bannerTimer);
   },function(){
